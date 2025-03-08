215913f575468879715a7a1784e52473024bc799
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-09 01:48:44 +0800

    fix(bump.sh): double quotes

--- bump.sh
+++ bump.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
-npm i monaco-editor@$1 && npm run build
+npm i "monaco-editor@$1" && npm run build
 if [[ $? -eq 0 ]]
 then
 	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
@@ -7,7 +7,7 @@ then
 	git add -A
 	git commit -m "$1"
 	git push
-	git tag $1
-	git push origin $1
-	npm publish --tag ${2-latest}
+	git tag "$1"
+	git push origin "$1"
+	npm publish --tag "${2-latest}"
 fi
